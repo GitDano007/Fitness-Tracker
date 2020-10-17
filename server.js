88af9edf354a890aa33d20ed51960ca34d684fc4
Dano Seese <dpseese@yahoo.com>
2020-10-17 12:06:12 -0700

    updates

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://PalmSprings:palmsprings007@cluster0.cexlj.mongodb.net/workout_db?retryWrites=true&w=majority", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+
+);
+
+//Routes
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+//most recent workout
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({}).then(data => {
+    res.json(data);
+  });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      exercises: req.body
+    }
+  }).then(data => {
+    res.json(data);
+  });
+});
+
+
+app.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body).then(data => {
+    res.json(data);
+  });
+});
+
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});
